9cd17cab7bc9cbf65926e776b6a1f4529f8f48e9
parki052 <parki052@umn.edu>
2024-01-17 20:09:56 -0600

    finalizing input queue system and moving all actions besides continuous movement to go through the input queue

--- Assets/ControllerScripts/freeze_controller.cs
+++ Assets/ControllerScripts/freeze_controller.cs
@@ -5,11 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 
 public class freeze_controller : MonoBehaviour
 {
-
     //unity objects
     private Rigidbody2D rigidBody;
     private Animator animator;
@@ -24,21 +24,24 @@ public class freeze_controller : MonoBehaviour
     private string punchLeftAnim = "freeze_punch_left";
     private string defendAnim = "freeze_defend";
     private string rollAnim = "freeze_roll";
+    private string runAttackAnim = "freeze_run_attack";
 
-    //movement speeds
+    //speed of movement during actions
     public const float walkSpeed = 2.3f;
     public const float runSpeed = 5.0f;
     public const float runStopSpeed = 3f;
     public const float punchSpeed = 0.17f;
 
-    //action durations (in milliseconds)
+    //speed of animations, and the calculated value of how long in ms an animation will take
+    public const float runAttackSpeedMultiplier = 1f;
     public const float defendSpeedMultiplier = 0.2f;
-    public int defendAnim_ms;
     public const float punchSpeedMultiplier = 0.85f;
-    public int punchAnim_ms;
     public const float rollSpeedMultiplier = 0.7f;
-    public int rollAnim_ms;
-    public int runStopAnim_ms = 250; //don't need to calculate since it's a 1 frame animation
+    private int defendAnim_ms;
+    private int punchAnim_ms;
+    private int rollAnim_ms;
+    private int runStopAnim_ms = 250; //don't need to calculate since it's a 1 frame animation
+    private int runAttackAnim_ms;
 
     //character state
     private bool facingRight = true;
@@ -50,7 +53,7 @@ public class freeze_controller : MonoBehaviour
     private bool movementBlocked = false;
 
     private List<QueuedInput> inputQueue;
-    private float inputTimeoutAfter = 0.5f;
+    private float inputTimeoutAfter = 0.5f; //how long an input will stay in the queue before being removed
 
     private void Awake()
     {
@@ -60,13 +63,15 @@ public class freeze_controller : MonoBehaviour
 
     private void OnEnable()
     {
-        inputControls.Player.attack.started += async (c) => await HandlePlayerInput(ActionInput.Attack);
-        inputControls.Player.defend.started += async (c) => await HandlePlayerInput(ActionInput.Defend);
-        inputControls.Player.jump.started += async (c) => await HandlePlayerInput(ActionInput.Jump);
-        inputControls.Player.direction_up.started += async (c) => await HandlePlayerInput(ActionInput.Up);
-        inputControls.Player.direction_down.started += async (c) => await HandlePlayerInput(ActionInput.Down);
-        inputControls.Player.direction_left.started += async (c) => await HandlePlayerInput(ActionInput.Left);
-        inputControls.Player.direction_right.started += async (c) => await HandlePlayerInput(ActionInput.Right);
+        //register listeners for input events (single button, fire and forget, NOT continuously held buttons as with WASD movement
+        //when an input event starts, add the input to the input queue
+        inputControls.Player.attack.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Attack, movementBlocked));
+        inputControls.Player.defend.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Defend, movementBlocked));
+        inputControls.Player.jump.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Jump, movementBlocked));
+        inputControls.Player.direction_up.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Up, movementBlocked));
+        inputControls.Player.direction_down.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Down, movementBlocked));
+        inputControls.Player.direction_left.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Left, movementBlocked));
+        inputControls.Player.direction_right.started += (c) => inputQueue.Add(new QueuedInput(Time.time, ActionInput.Right, movementBlocked));
 
         inputControls.Player.Enable();
     }
@@ -77,80 +82,42 @@ public class freeze_controller : MonoBehaviour
         rigidBody.velocity = Vector2.zero;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rigidBody = GetComponent<Rigidbody2D>();
 
-
         //set the animation's speed then save the animation's calculated duration
         animator.SetFloat("punchSpeed", punchSpeedMultiplier);
         animator.SetFloat("defendSpeed", defendSpeedMultiplier);
         animator.SetFloat("rollSpeed", rollSpeedMultiplier);
+        animator.SetFloat("runAttackSpeed", runAttackSpeedMultiplier);
 
         punchAnim_ms = GetAnimationDurations_Ms(punchLeftAnim, punchSpeedMultiplier);
         defendAnim_ms = GetAnimationDurations_Ms(defendAnim, defendSpeedMultiplier);
         rollAnim_ms = GetAnimationDurations_Ms(rollAnim, rollSpeedMultiplier);
-
+        runAttackAnim_ms = GetAnimationDurations_Ms(runAttackAnim, runAttackSpeedMultiplier);
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
 
-    private void FixedUpdate()
+    private async void FixedUpdate()
     {
-        HandleInputQueue();
+        //the input queue is handled, and if needed, actions/animations/movement is triggered
+        await HandleInputQueue();
+
+        //after handling the input queue, standing/running/walking animations and movement are handled
         HandlePlayerMovement();
     }
-
-    private async Task HandlePlayerInput(ActionInput input)
-    {
-        inputQueue.Add(new QueuedInput(Time.time, input, movementBlocked));
-        switch (input)
-        {
-            case ActionInput.Attack:
-                if (!movementBlocked)
-                {
-                    movementBlocked = true;
-
-                    var anim = RNG(38) ? punchLeftAnim : punchRightAnim;
-                    animator.Play(anim);
-                    rigidBody.velocity = new Vector2(facingRight ? punchSpeed : -punchSpeed, 0);
-              
-                    await Task.Delay(punchAnim_ms);
-                    movementBlocked = false;
-                }
-                break;
-            case ActionInput.Jump:
-                break;
-            case ActionInput.Defend:
-                if (!movementBlocked)
-                {
-                    movementBlocked = true;
-                    rigidBody.velocity = new Vector2(0, 0);
-                    animator.Play(defendAnim);
-
-                    await Task.Delay(defendAnim_ms);
-                    animator.Play(standAnim);
-                    await Task.Delay(50);
-                    movementBlocked = false;
-
-                }
-                break;
-            default: break;
-        }
-    }
-
-    private async void HandleInputQueue()
+    private async Task HandleInputQueue()
     {
         //trim out inputs that happened too long ago
-        foreach(var input in inputQueue.ToList())
+        foreach (var input in inputQueue.ToList())
         {
-            if(Time.time - input.InputTime > inputTimeoutAfter)
+            if (Time.time - input.InputTime > inputTimeoutAfter)
             {
                 inputQueue.Remove(input);
             }
@@ -167,26 +134,27 @@ public class freeze_controller : MonoBehaviour
         */
 
         bool foundAction = false;
-        
-        if (inputQueue.Count >= 3) // 1. special moves section
+
+        // 1. special moves section
+        if (inputQueue.Count >= 3)
         {
             var lastThreeInputs = inputQueue.TakeLast(3);
             var inputList = "";
-            foreach(var input in lastThreeInputs)
+            foreach (var input in lastThreeInputs)
             {
                 inputList += input.Input.ToString() + " ";
-                inputQueue.Remove(input);
             }
             Debug.Log("Hit special moves. Current moves: " + inputList);
         }
-        if (inputQueue.Count >= 2 && !foundAction) // 2. detect running section
+        // 2. detect running section
+        if (inputQueue.Count >= 2 && !foundAction)
         {
             var lastTwoInputs = inputQueue.TakeLast(2);
 
-            if(lastTwoInputs.All(input => input.Input == ActionInput.Left) || lastTwoInputs.All(input => input.Input == ActionInput.Right))
+            if (lastTwoInputs.All(input => input.Input == ActionInput.Left) || lastTwoInputs.All(input => input.Input == ActionInput.Right))
             {
                 if (!movementBlocked)
-                {                  
+                {
                     runningRight = lastTwoInputs.Last().Input == ActionInput.Right;
 
                     isRunning = true;
@@ -195,15 +163,20 @@ public class freeze_controller : MonoBehaviour
                 }
             }
         }
-        if(inputQueue.Count >= 2 && !foundAction) // 3. detect chained attack / defend / jump section
+        // 3. detect chained attack / defend / jump section
+        if (inputQueue.Count >= 2 && !foundAction)
         {
 
         }
-        if(inputQueue.Count >= 1 && !foundAction) // 4. process singular inputs
+        // 4. process singular inputs
+        if (inputQueue.Count >= 1 && !foundAction)
         {
-            if (isRunning) // inputs while running
+            var lastInput = inputQueue.Last().Input;
+            //handle singular input while running
+            if (isRunning)
             {
-                if(inputQueue.Last().Input == ActionInput.Left && runningRight || inputQueue.Last().Input == ActionInput.Right && !runningRight)//handle stopping running
+                //handle stopping running
+                if (lastInput == ActionInput.Left && runningRight || lastInput == ActionInput.Right && !runningRight)
                 {
                     bool goingRight = runningRight;
 
@@ -217,21 +190,74 @@ public class freeze_controller : MonoBehaviour
                     movementBlocked = false;
                     inputQueue.Clear();
                 }
-                else if(inputQueue.Last().Input == ActionInput.Defend)
+                //handle rolling
+                else if (lastInput == ActionInput.Defend)
                 {
-                    bool goingRight = runningRight;
-
+                    StopMovement();
                     isRunning = false;
                     movementBlocked = true;
                     animator.Play(rollAnim);
-                    rigidBody.velocity = new Vector2(goingRight ? runSpeed : -runSpeed, 0);
+                    rigidBody.velocity = new Vector2(runningRight ? runSpeed : -runSpeed, 0);
                     await Task.Delay(rollAnim_ms);
                     StopMovement();
                     movementBlocked = false;
                     inputQueue.Clear();
-
+                }
+                //handle running attack
+                else if (lastInput == ActionInput.Attack)
+                {
+                    StopMovement();
+                    isRunning = false;
+                    movementBlocked = true;
+                    animator.Play(runAttackAnim);
+                    rigidBody.velocity = new Vector2(runningRight ? runSpeed : -runSpeed, 0);
+                    await Task.Delay((int)(runAttackAnim_ms * .45));
+                    rigidBody.velocity = new Vector2(0, 0);
+                    await Task.Delay((int)(runAttackAnim_ms * .55));
+                    movementBlocked = false;
+                    inputQueue.Clear();
+                }
+                //handle running jump
+                else if (lastInput == ActionInput.Jump)
+                {
 
                 }
+            }
+            //handle singular input while not running and input is not blocked
+            else if (!movementBlocked)
+            {
+                //handle singular attack
+                if (lastInput == ActionInput.Attack)
+                {
+                    movementBlocked = true;
+
+                    var anim = RNG(38) ? punchLeftAnim : punchRightAnim;
+                    animator.Play(anim);
+                    rigidBody.velocity = new Vector2(facingRight ? punchSpeed : -punchSpeed, 0);
+
+                    await Task.Delay(punchAnim_ms);
+                    movementBlocked = false;
+                    inputQueue.Clear();
+                }
+                //handle singular jump
+                else if (lastInput == ActionInput.Jump)
+                {
+
+                }
+                //handle singular defend
+                else if (lastInput == ActionInput.Defend)
+                {
+                    movementBlocked = true;
+                    rigidBody.velocity = new Vector2(0, 0);
+                    animator.Play(defendAnim);
+
+                    await Task.Delay(defendAnim_ms);
+                    animator.Play(standAnim);
+                    await Task.Delay(50);
+                    movementBlocked = false;
+                    inputQueue.Clear();
+                }
+
             }
         }
     }
@@ -245,9 +271,15 @@ public class freeze_controller : MonoBehaviour
         facingRight = !facingRight;
     }
 
+    /// <summary>
+    /// Returns a bool calculated by the given percent chance (an int between 1-100 inclusive) of being true 
+    /// </summary>
+    /// <param name="percentChance"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     private bool RNG(int percentChance)
     {
-        if(percentChance < 1 || percentChance > 100)
+        if (percentChance < 1 || percentChance > 100)
         {
             throw new Exception("GetRandom() requires percent chance to be between 1 and 100.");
         }
@@ -266,9 +298,9 @@ public class freeze_controller : MonoBehaviour
 
             if (isRunning) //are we running around
             {
-                if((runningRight && !facingRight) || (!runningRight && facingRight)) 
-                { 
-                    Flip(); 
+                if ((runningRight && !facingRight) || (!runningRight && facingRight))
+                {
+                    Flip();
                 }
                 animator.Play(runAnim);
                 newVelocity = new Vector2(facingRight ? runSpeed : -runSpeed, (y * walkSpeed) * .7f);
@@ -283,12 +315,12 @@ public class freeze_controller : MonoBehaviour
                 animator.Play(walkAnim);
                 newVelocity = new Vector2(x * walkSpeed, y * walkSpeed);
             }
-            else if(x == 0 && y == 0) //are we standing still
+            else if (x == 0 && y == 0) //are we standing still
             {
                 animator.Play(standAnim);
             }
 
-            rigidBody.velocity = newVelocity; 
+            rigidBody.velocity = newVelocity;
         }
     }
 
